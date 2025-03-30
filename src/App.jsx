77184522ec9d605ba5ac8c5ca953339d9a58f5da
cuvirucho <157436675+cuvirucho@users.noreply.github.com>
2025-03-30 18:32:20 -0500

    trs

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     { id: 1, code: 'CUPO1', description: '10% de descuento' ,img: 'https://res.cloudinary.com/db8e98ggo/image/upload/v1735948412/PROSESANDO_4_rdguoq.gif'},
     { id: 2, code: 'CUPO2', description: '3x2 en desayunos',img: 'https://res.cloudinary.com/db8e98ggo/image/upload/v1735996017/PROSESANDO_11_tdu7dl.gif' },
     { id: 3, code: 'CUPO3', description: 'Cafe gratis con cualquier tigrillo' ,img: 'https://res.cloudinary.com/db8e98ggo/image/upload/v1735995406/PROSESANDO_10_gcxtaz.gif'},
-    { id: 3, code: 'CUPO3', description: 'Por compras mayoreas a $10 reciben un regalo' ,img: 'https://res.cloudinary.com/db8e98ggo/image/upload/v1735998680/PROSESANDO_13_faheei.gif'},
+    { id: 3, code: 'CUPO3', description: 'Por compras mayoreas a $10 reciben un regalo' ,img: 'https://res.cloudinary.com/db8e98ggo/image/upload/v1735949983/PROSESANDO_9_oaatim.gif'},
     { id: 3, code: 'CUPO3', description: 'En pareja el segudo capuchino es a 0.5ctvs' ,img: 'https://res.cloudinary.com/db8e98ggo/image/upload/v1735948964/PROSESANDO_5_dtun5r.gif'},
   ];
   return (
@@ -25,7 +25,7 @@ function App() {
  
    </section>
   
-   <h2  className='tilecupo'    >Cuponera valida unicamente para los alupnos de Mykar</h2>
+   <h2  className='tilecupo'    >Cuponera valida unicamente para los alupnos de mykar</h2>
 
 
 
